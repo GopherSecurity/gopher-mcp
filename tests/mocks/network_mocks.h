2d9aa5bc359fa8c7eece613d72d18c57c7a42ef8
caleb2h <caleb2h@nobody.im>
2025-08-06 11:04:47 -0700

    Fix mock interfaces for test compatibility (#25)
    - Update MockIoHandle to match current IoHandle interface
    - Fix IoResult return types (IoVoidResult -> IoResult<int>)
    - Add missing virtual methods (duplicate, lastRoundTripTime, etc.)
    - Update MockSocketInterface to match SocketInterface API
    - Fix MockFilterChainFactory to return bool instead of void
    - Add proper default behaviors for close() and connect()

--- tests/mocks/network_mocks.h
+++ tests/mocks/network_mocks.h
@@ -32,53 +32,46 @@ public:
               (Buffer& buffer), 
               (override));
   MOCK_METHOD(network::IoCallResult, sendmsg, 
-              (const ConstRawSlice* slices, size_t num_slices, uint32_t self_port, 
-               const network::Address::Ip& peer_address), 
+              (const ConstRawSlice* slices, size_t num_slices, int flags,
+               const network::Address::Ip* self_ip, const network::Address::Instance& peer_address), 
               (override));
   MOCK_METHOD(network::IoCallResult, recvmsg, 
               (RawSlice* slices, size_t num_slices, uint32_t self_port, 
-               network::RecvMsgOutput& output), 
+               const network::UdpSaveCmsgConfig& save_cmsg_config, network::RecvMsgOutput& output), 
               (override));
   MOCK_METHOD(network::IoCallResult, recvmmsg, 
-              (RawSlice* slices, size_t num_slices, uint32_t self_port, 
-               network::RecvMsgOutput& output), 
+              (std::vector<RawSlice>& slices, uint32_t self_port, 
+               const network::UdpSaveCmsgConfig& save_cmsg_config, network::RecvMsgOutput& output), 
               (override));
   MOCK_METHOD(network::IoVoidResult, close, (), (override));
   MOCK_METHOD(bool, isOpen, (), (const, override));
   MOCK_METHOD(network::os_fd_t, fd, (), (const, override));
-  MOCK_METHOD(network::IoVoidResult, shutdown, (int how), (override));
-  MOCK_METHOD(network::IoVoidResult, bind, 
+  MOCK_METHOD(network::IoResult<int>, shutdown, (int how), (override));
+  MOCK_METHOD(network::IoResult<int>, bind, 
               (const network::Address::InstanceConstSharedPtr& address), 
               (override));
-  MOCK_METHOD(network::IoVoidResult, listen, (int backlog), (override));
-  MOCK_METHOD(network::IoVoidResult, connect, 
+  MOCK_METHOD(network::IoResult<int>, listen, (int backlog), (override));
+  MOCK_METHOD(network::IoResult<int>, connect, 
               (const network::Address::InstanceConstSharedPtr& address), 
               (override));
-  MOCK_METHOD(network::IoVoidResult, setBlocking, (bool blocking), (override));
-  MOCK_METHOD(optional<network::Address::InstanceConstSharedPtr>, localAddress, 
-              (), (override));
-  MOCK_METHOD(optional<network::Address::InstanceConstSharedPtr>, peerAddress, 
-              (), (override));
-  MOCK_METHOD(network::SocketPtr, accept, 
-              (struct sockaddr* addr, socklen_t* addrlen), 
+  MOCK_METHOD(network::IoResult<int>, setBlocking, (bool blocking), (override));
+  MOCK_METHOD(network::IoResult<network::Address::InstanceConstSharedPtr>, localAddress, 
+              (), (const, override));
+  MOCK_METHOD(network::IoResult<network::Address::InstanceConstSharedPtr>, peerAddress, 
+              (), (const, override));
+  MOCK_METHOD(network::IoResult<network::IoHandlePtr>, accept, 
+              (), 
               (override));
-  MOCK_METHOD(network::IoVoidResult, setOption, 
+  MOCK_METHOD(network::IoResult<int>, setSocketOption, 
               (int level, int optname, const void* optval, socklen_t optlen), 
               (override));
-  MOCK_METHOD(network::IoVoidResult, getOption, 
+  MOCK_METHOD(network::IoResult<int>, getSocketOption, 
               (int level, int optname, void* optval, socklen_t* optlen), 
               (const, override));
-  MOCK_METHOD(network::IoVoidResult, ioctl, 
+  MOCK_METHOD(network::IoResult<int>, ioctl, 
               (unsigned long request, void* argp), 
               (override));
-  MOCK_METHOD(network::IoVoidResult, waitForRead, 
-              (event::FileReadyCb cb, event::FileTriggerType trigger, 
-               event::Dispatcher& dispatcher), 
-              (override));
-  MOCK_METHOD(network::IoVoidResult, waitForWrite, 
-              (event::FileReadyCb cb, event::FileTriggerType trigger, 
-               event::Dispatcher& dispatcher), 
-              (override));
+  // Note: waitForRead and waitForWrite were removed from the interface
   MOCK_METHOD(void, initializeFileEvent, 
               (event::Dispatcher& dispatcher, event::FileReadyCb cb, 
                event::FileTriggerType trigger, uint32_t events), 
@@ -86,19 +79,23 @@ public:
   MOCK_METHOD(void, activateFileEvents, (uint32_t events), (override));
   MOCK_METHOD(void, enableFileEvents, (uint32_t events), (override));
   MOCK_METHOD(void, resetFileEvents, (), (override));
-  MOCK_METHOD(network::IoVoidResult, enableFileEvent, 
-              (event::FileReadyType events), 
-              (override));
-  MOCK_METHOD(network::SupportedSocketProtocols, supportedProtocols, 
+  // Note: enableFileEvent was removed from the interface
+  MOCK_METHOD(optional<std::string>, interfaceName, 
               (), (const, override));
-  MOCK_METHOD(network::IoVoidResult, setUdpSaveCmsgConfig, 
-              (const network::UdpSaveCmsgConfig& config), 
-              (override));
+  MOCK_METHOD(network::IoHandlePtr, duplicate, (), (override));
+  MOCK_METHOD(optional<std::chrono::milliseconds>, lastRoundTripTime, (), (const, override));
+  MOCK_METHOD(void, configureInitialCongestionWindow, 
+              (uint64_t bandwidth_bits_per_sec, std::chrono::microseconds rtt), (override));
   
   MockIoHandle() {
     // Set default behaviors
     ON_CALL(*this, isOpen()).WillByDefault(Return(true));
     ON_CALL(*this, fd()).WillByDefault(Return(0));
+    ON_CALL(*this, connect(_)).WillByDefault(Return(network::IoResult<int>::success(0)));
+    // Create a properly initialized IoVoidResult for close()
+    ON_CALL(*this, close()).WillByDefault([]() -> network::IoVoidResult {
+      return network::IoVoidResult::success();
+    });
   }
 };
 
@@ -107,14 +104,9 @@ class MockSocketInterface : public network::SocketInterface {
 public:
   MOCK_METHOD(network::IoResult<network::os_fd_t>, socket, 
               (network::SocketType type, network::Address::Type addr_type,
-               network::Address::IpVersion version, bool socket_v6only),
+               optional<network::Address::IpVersion> version, bool socket_v6only),
               (override));
-  MOCK_METHOD(network::IoResult<network::os_fd_t>, socket,
-              (network::SocketType type, network::Address::Type addr_type),
-              (override));
-  MOCK_METHOD(network::IoResult<network::os_fd_t>, accept,
-              (network::os_fd_t fd, sockaddr* addr, socklen_t* addrlen),
-              (override));
+  // Note: accept is not in SocketInterface, it's in IoHandle
   MOCK_METHOD(network::IoResult<int>, socketPair,
               (network::SocketType type, network::os_fd_t fds[2]),
               (override));
@@ -122,20 +114,32 @@ public:
               (network::os_fd_t fd, bool socket_v6only, optional<int> domain),
               (override));
   MOCK_METHOD(network::IoResult<int>, close, (network::os_fd_t fd), (override));
-  MOCK_METHOD(network::SocketPtr, createSocket,
-              (const network::SocketOptionsSharedPtr& options,
-               const network::Address::InstanceConstSharedPtr& address),
+  MOCK_METHOD(network::IoResult<network::os_fd_t>, duplicate, (network::os_fd_t fd), (override));
+  MOCK_METHOD(network::IoResult<int>, setFileFlags, (network::os_fd_t fd, int flags), (override));
+  MOCK_METHOD(network::IoResult<int>, getFileFlags, (network::os_fd_t fd), (override));
+  MOCK_METHOD(network::IoResult<int>, setsockopt,
+              (network::os_fd_t fd, int level, int optname, const void* optval, socklen_t optlen),
               (override));
+  MOCK_METHOD(network::IoResult<int>, getsockopt,
+              (network::os_fd_t fd, int level, int optname, void* optval, socklen_t* optlen),
+              (override));
+  MOCK_METHOD(network::IoResult<int>, bind, (network::os_fd_t fd, const network::Address::Instance& addr), (override));
+  MOCK_METHOD(network::IoResult<int>, listen, (network::os_fd_t fd, int backlog), (override));
+  MOCK_METHOD(network::IoResult<network::os_fd_t>, accept, (network::os_fd_t fd, struct sockaddr* addr, socklen_t* addrlen), (override));
+  MOCK_METHOD(network::IoResult<int>, connect, (network::os_fd_t fd, const network::Address::Instance& addr), (override));
+  MOCK_METHOD(network::IoResult<int>, shutdown, (network::os_fd_t fd, int how), (override));
+  MOCK_METHOD(network::IoResult<network::Address::InstanceConstSharedPtr>, localAddress, (network::os_fd_t fd), (override));
+  MOCK_METHOD(network::IoResult<network::Address::InstanceConstSharedPtr>, peerAddress, (network::os_fd_t fd), (override));
+  MOCK_METHOD(network::IoResult<int>, ioctl, (network::os_fd_t fd, unsigned long request, void* argp), (override));
+  MOCK_METHOD(optional<std::string>, interfaceName, (network::os_fd_t fd), (override));
+  MOCK_METHOD(bool, supportsSocketOption, (int level, int optname), (const, override));
+  MOCK_METHOD(const std::string&, platformName, (), (const, override));
   MOCK_METHOD(bool, supportsUdpGro, (), (const, override));
-  MOCK_METHOD(network::ErrorCounters&, errorCounters, (), (override));
   
   MockSocketInterface() {
     // Setup default behaviors for common operations
-    ON_CALL(*this, socket(_, _))
-        .WillByDefault(Return(network::IoResult<network::os_fd_t>(10)));  // Return a fake fd
-    
     ON_CALL(*this, socket(_, _, _, _))
-        .WillByDefault(Return(network::IoResult<network::os_fd_t>(10)));  // Return a fake fd
+        .WillByDefault(Return(network::IoResult<network::os_fd_t>::success(10)));  // Return a fake fd
     
     ON_CALL(*this, ioHandleForFd(_, _, _))
         .WillByDefault([]() {
@@ -143,10 +147,14 @@ public:
         });
     
     ON_CALL(*this, close(_))
-        .WillByDefault(Return(network::IoResult<int>(0)));
+        .WillByDefault(Return(network::IoResult<int>::success(0)));
     
     ON_CALL(*this, supportsUdpGro())
         .WillByDefault(Return(false));
+        
+    static std::string platform_name = "test";
+    ON_CALL(*this, platformName())
+        .WillByDefault(testing::ReturnRef(platform_name));
   }
 };
 
@@ -172,18 +180,18 @@ public:
     ON_CALL(*this, failureReason()).WillByDefault(Return(""));
     ON_CALL(*this, canFlushClose()).WillByDefault(Return(true));
     ON_CALL(*this, connect(_)).WillByDefault(Return(makeVoidSuccess()));
-    ON_CALL(*this, doRead(_)).WillByDefault(Return(TransportIoResult{TransportIoAction::KeepOpen, 0, false}));
-    ON_CALL(*this, doWrite(_, _)).WillByDefault(Return(TransportIoResult{TransportIoAction::KeepOpen, 0, false}));
+    ON_CALL(*this, doRead(_)).WillByDefault(Return(TransportIoResult{TransportIoResult::CONTINUE, 0, false}));
+    ON_CALL(*this, doWrite(_, _)).WillByDefault(Return(TransportIoResult{TransportIoResult::CONTINUE, 0, false}));
   }
 };
 
 // Mock FilterChainFactory for testing
 class MockFilterChainFactory : public network::FilterChainFactory {
 public:
-  MOCK_METHOD(void, createFilterChain, (network::FilterManager& filter_manager), (const, override));
+  MOCK_METHOD(bool, createFilterChain, (network::FilterManager& filter_manager), (const, override));
   
   MockFilterChainFactory() {
-    ON_CALL(*this, createFilterChain(_)).WillByDefault([](network::FilterManager&) {});
+    ON_CALL(*this, createFilterChain(_)).WillByDefault([](network::FilterManager&) { return true; });
   }
 };
 
