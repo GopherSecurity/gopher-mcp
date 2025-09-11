a6afb11bf7ca900058b30c074b8e87e23c31dff7
Divyansh Ingle <divyanshingle@gmail.com>
2025-09-11 19:52:19 +0530

    Update Ruby types index with proper module structure (#110)

--- sdk/ruby/lib/mcp_filter_sdk/types/index.rb
+++ sdk/ruby/lib/mcp_filter_sdk/types/index.rb
@@ -3,6 +3,6 @@ require 'mcp_filter_sdk/types/mcp_types'
 module McpFilterSdk
   module Types
     # Re-export all types for easy access
-    include McpTypes
+    # All types are already defined in mcp_types.rb
   end
 end
